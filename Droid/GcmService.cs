db7f065da992eca97f0ba9f08062586af0ca8dbe
Nick Patterson <nkpatterson@live.com>
2016-02-02 10:11:36 -0500

    Initial

---
+++ Droid/GcmService.cs
@@ -0,0 +1,147 @@
+using Gcm.Client;
+using Microsoft.WindowsAzure.MobileServices;
+using Android.App;
+using Android.Content;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Android.Util;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System;
+
+[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECEIVE")]
+
+//GET_ACCOUNTS is only needed for android versions 4.0.3 and below
+[assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
+[assembly: UsesPermission(Name = "android.permission.INTERNET")]
+[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
+namespace nkpnotificationsdemo.Droid
+{
+    [BroadcastReceiver(Permission = Gcm.Client.Constants.PERMISSION_GCM_INTENTS)]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_MESSAGE }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_REGISTRATION_CALLBACK }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_LIBRARY_RETRY }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    public class PushHandlerBroadcastReceiver : GcmBroadcastReceiverBase<GcmService>
+    {
+        public static string[] SENDER_IDS = new string[] { "69129111524" };
+    }
+
+    [Service]
+    public class GcmService : GcmServiceBase
+    {
+        public static string RegistrationID { get; private set; }
+
+        public GcmService()
+            : base(PushHandlerBroadcastReceiver.SENDER_IDS)
+        { }
+
+        protected override void OnRegistered(Context context, string registrationId)
+        {
+            Log.Verbose("PushHandlerBroadcastReceiver", "GCM Registered: " + registrationId);
+            RegistrationID = registrationId;
+
+            createNotification("GcmService Registered...", "The device has been Registered, Tap to View!");
+
+            var push = TodoItemManager.DefaultManager.CurrentClient.GetPush();
+
+            MainActivity.CurrentActivity.RunOnUiThread(() => Register(push, null));
+        }
+
+        protected override bool OnRecoverableError(Context context, string errorId)
+        {
+            Log.Info("PushHandlerBroadcastReceiver", "Recoverable error occurred: " + errorId);
+            return true;
+        }
+
+        protected override void OnMessage(Context context, Intent intent)
+        {
+            Log.Info("PushHandlerBroadcastReceiver", "GCM Message Received!");
+
+            var msg = new StringBuilder();
+
+            if (intent != null && intent.Extras != null)
+            {
+                foreach (var key in intent.Extras.KeySet())
+                    msg.AppendLine(key + "=" + intent.Extras.Get(key).ToString());
+            }
+
+            //Store the message
+            var prefs = GetSharedPreferences(context.PackageName, FileCreationMode.Private);
+            var edit = prefs.Edit();
+            edit.PutString("last_msg", msg.ToString());
+            edit.Commit();
+
+            string message = intent.Extras.GetString("message");
+            if (!string.IsNullOrEmpty(message))
+            {
+                createNotification("New todo item!", "Todo item: " + message);
+                return;
+            }
+
+            string msg2 = intent.Extras.GetString("msg");
+            if (!string.IsNullOrEmpty(msg2))
+            {
+                createNotification("New hub message!", msg2);
+                return;
+            }
+
+            createNotification("Unknown message details", msg.ToString());
+        }
+        protected override void OnUnRegistered(Context context, string registrationId)
+        {
+            Log.Error("PushHandlerBroadcastReceiver", "Unregistered RegisterationId : " + registrationId);
+        }
+
+        protected override void OnError(Context context, string errorId)
+        {
+            Log.Error("PushHandlerBroadcastReceiver", "GCM Error: " + errorId);
+        }
+
+        void createNotification(string title, string desc)
+        {
+            //Create notification
+            var notificationManager = GetSystemService(Context.NotificationService) as NotificationManager;
+
+            //Create an intent to show ui
+            var uiIntent = new Intent(this, typeof(MainActivity));
+
+            //Create the notification
+            var notification = new Notification(Android.Resource.Drawable.SymActionEmail, title);
+
+            //Auto cancel will remove the notification once the user touches it
+            notification.Flags = NotificationFlags.AutoCancel;
+
+            //Set the notification info
+            //we use the pending intent, passing our ui intent over which will get called
+            //when the notification is tapped.
+            notification.SetLatestEventInfo(this, title, desc, PendingIntent.GetActivity(this, 0, uiIntent, 0));
+
+            //Show the notification
+            notificationManager.Notify(1, notification);
+        }
+
+        public async void Register(Push push, IEnumerable<string> tags)
+        {
+            try
+            {
+                const string templateBodyGCM = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+
+                JObject templates = new JObject();
+                templates["genericMessage"] = new JObject
+                {
+                  {"body", templateBodyGCM}
+                };
+
+                await push.RegisterAsync(RegistrationID, templates);
+                Log.Info("Push Installation Id", push.InstallationId.ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                Debugger.Break();
+            }
+        }
+    }
+}
---
+++ nkpnotificationsdemo/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace nkpnotificationsdemo
+{
+	public static class Constants
+	{
+        // Replace strings with your mobile services and gateway URLs.
+        public static string ApplicationURL = @"https://nkpnotificationsdemo.azurewebsites.net";
+        //public static string ApplicationURL = "http://localhost:51033/";
+
+    }
+}
+
